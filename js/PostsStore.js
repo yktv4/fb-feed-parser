751473a750306e946bf13423816434c77990aad8
Igor Yakutovich <turboexe@gmail.com>
2015-05-30 03:16:40 +0300

    implemented the ability to fetch more than 250 posts using multiple calls

--- js/PostsStore.js
+++ js/PostsStore.js
@@ -1,21 +1,43 @@
 var PostsStore = Reflux.createStore({
     posts: [],
+    maxCallLimit: 250,
     init: function () {
         Actions.fetch.listen(this.fetchPosts)
     },
     fetchPosts: function (pageId, number) {
+        var promises = [];
         this.posts = [];
         this.trigger();
 
-        FB.api(
-            '/' + pageId + '/feed?limit=' + number,
-            function (response) {
-                if (response && !response.error) {
-                    this.posts = response.data;
-                    this.trigger();
-                }
-            }.bind(this)
-        );
+        if (number > this.maxCallLimit) {
+            var limits = (new Array(Math.floor(number / this.maxCallLimit))).join(' ').split(' ')
+                .map(function () {return this.maxCallLimit;}.bind(this));
+            number % this.maxCallLimit === 0 || limits.push(this.maxCallLimit - number);
+            promises = limits.map(function (el, idx) {
+                return this.fbApiCall(pageId, el, idx === 0 ? 0 : (idx * this.maxCallLimit)+1);
+            }.bind(this));
+        } else {
+            promises.push(this.fbApiCall(pageId, number, 0));
+        }
+
+        Promise.all(promises).then(function (results) {
+            this.posts = this.posts.concat.apply(this.posts, results);
+            this.trigger();
+        }.bind(this));
+    },
+    fbApiCall: function (pageId, limit, offset) {
+        return new Promise(function (resolve, reject) {
+            FB.api(
+                '/' + pageId + '/feed?offset=' + offset + '&limit=' + limit,
+                function (response) {
+                    if (!response || response.error) {
+                        reject(response.error || 'no response received');
+                    } else {
+                        resolve(response.data);
+                    }
+                }.bind(this)
+            );
+        });
     },
     get: function () {
         return this.posts;
