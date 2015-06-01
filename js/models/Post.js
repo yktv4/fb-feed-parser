8153e4388908d34733cc770a5ba3a02211ff0675
Igor Yakutovich <turboexe@gmail.com>
2015-06-01 18:47:57 +0300

    now displaying a picture next to post

--- js/models/Post.js
+++ js/models/Post.js
@@ -1,18 +1,21 @@
 var Post = Backbone.Model.extend({
-    created_time: null,
-    message: null,
-    likes: null,
-    shares: null,
-
     parse: function (response, options) {
         return {
+            object_id: response.object_id,
             created_time: response.created_time,
             message: response.message || '',
             likes: response.likes.summary.total_count,
-            shares: (response.shares && response.shares.count) || 0
+            shares: (response.shares && response.shares.count) || 0,
+            picture: {
+                thumb: response.picture,
+                full: response.picture && 'http://graph.facebook.com/' + response.object_id + '/picture'
+            }
         }
     },
     getFormattedDate: function () {
         return moment(this.get('created_time')).format('YYYY-MM-DD HH-mm');
+    },
+    pictureExists: function () {
+        return this.get('picture').thumb !== undefined;
     }
 });
--- jsx/PostGridItem.jsx
+++ jsx/PostGridItem.jsx
@@ -3,7 +3,16 @@ var PostGridItem = React.createClass({
         return (
             <tr>
                 <td className="date-cell">{ this.props.post.getFormattedDate() }</td>
-                <td>{ this.props.post.get('message') }</td>
+                <td>
+                    { this.props.post.get('message') }
+                    { this.props.post.pictureExists()
+                        ? <div>
+                            <a href={ this.props.post.get('picture').full } target="_blank">
+                              <img className="fb-thumb-image" src={ this.props.post.get('picture').thumb } alt="" />
+                            </a>
+                          </div>
+                        : null }
+                </td>
                 <td>{ this.props.post.get('likes') }</td>
                 <td>{ this.props.post.get('shares') }</td>
             </tr>
